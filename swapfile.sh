7b80a6ae913f21009c56cc7ee6ac619d4f75bd60
caesar <williamcesar505@gmail.com>
2017-08-28 19:33:54 -0300

    firt commit

---
+++ swapfile.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Input how much swap you want, in Gigabytes: "
+read number_of_swap
+number=$((number_of_swap\*1038336))
+rm /swapfile
+touch /swapfile
+dd if=/dev/zero of=/swapfile bs=1024 count=$number
+echo $number
+chmod 0600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile          swap            swap    defaults        0 0" >> /etc/fstab
